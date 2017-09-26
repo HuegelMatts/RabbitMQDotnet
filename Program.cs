a37b928b8393fb0f685a3c1c9d59c32367ba255e
Thiago Loureiro <thiagoguaru@me.com>
2017-09-26 20:40:26 +0200

    Implemented Methods - Create Queue - Read Message - Read MessageList - Write Message - Message Count

--- Program.cs
+++ Program.cs
@@ -1,17 +1,80 @@
 ﻿using Microsoft.Practices.Unity;
+using RabbitMQ.Client;
 using RabbitMQApp.DI;
 using RabbitMQApp.RabbitMQHelper;
+using System;
+using System.Threading;
 
 namespace RabbitMQApp
 {
     internal class Program
     {
-        private static void Main(string[] args)
+        private static void Main()
         {
-            var container = UnityConfig.GetMainContainer();
-            var obj = container.Resolve<RabbitMqHelper>();
+            IConnection connection = null;
 
-            var conn = obj.GetConnectionFactory();
+            var queueName = Guid.NewGuid().ToString();
+
+            try
+            {
+                // Printing ASCII Art ;-)
+                AsciiArt.AsciiArt.Write("Rabbit MQ");
+
+                // Creating IOC Container
+                var container = UnityConfig.GetMainContainer();
+                var obj = container.Resolve<RabbitMqHelper>();
+
+                // Creating Connection and Connection Factory
+                var connectionFactory = obj.GetConnectionFactory();
+                connection = obj.CreateConnection(connectionFactory);
+
+                // Creating a New Queue
+                Console.WriteLine("Creating New Queue");
+                obj.CreateQueue(queueName, connection);
+                Console.WriteLine($"Queue Sucessfully Created: {queueName}");
+                Console.WriteLine(" ");
+
+                // Retrieving Message Count from Queue
+                Console.Write("Messages Count: ");
+                var messageCount = obj.RetrieveMessageCount(queueName, connection);
+                Console.WriteLine(messageCount.ToString());
+                Console.WriteLine(" ");
+
+                // Writing Messages to a Queue
+                for (int i = 0; i < 5; i++)
+                {
+                    var newmessage = $"New Message Generated on: {DateTime.Now:dd/MM/yyyy HH:mm:ss:fff}";
+
+                    obj.WriteMessageOnQueue(newmessage, queueName, connection);
+                    Console.WriteLine($"Message Successfully Written: {newmessage}");
+                    Thread.Sleep(20);
+                }
+                Console.WriteLine(" ");
+
+                // Retrieving One Message
+                Console.Write("Retrieving One Message, Message Text: ");
+                var message = obj.RetrieveSingleMessage(queueName, connection);
+                Console.WriteLine(message);
+                Console.WriteLine(" ");
+
+                // Retrieving Multiple Messages
+                var lstMessages = obj.RetrieveMessageList(queueName, connection);
+                foreach (var m in lstMessages)
+                {
+                    Console.WriteLine($"Message Text: {m} ");
+                }
+                Console.WriteLine(" ");
+            }
+            finally
+            {
+                if (connection != null)
+                {
+                    connection.Close();
+                    connection.Dispose();
+                }
+            }
+
+            Console.ReadKey();
         }
     }
 }
